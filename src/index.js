a473adbbc38543b601219014c4df8cb5de263506
Alex Lubchuk <a.lubchuk@brandslisten.com>
2018-02-01 19:22:41 +0200

    initial

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as redux from 'redux';
+import { connect } from 'react-redux';
+import '@alex-playground/api';
+import renderComponents from '@alex-playground/components';
+import init from '@alex-playground/containers';
+
+init('Stars');
